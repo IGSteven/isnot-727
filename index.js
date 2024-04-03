d8ecf6e2537baa7620465689eb8381a3b6484ec7
Steven A. Smith <me@igsteven.com>
2024-04-03 23:25:28 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 'use strict';
-const is272 = require('is-727');
+const is727 = require('is-727');
 
 export default function(value) {
-  if (!is272(value)){
+  if (!is727(value)){
     return true
   } else {
     return false
